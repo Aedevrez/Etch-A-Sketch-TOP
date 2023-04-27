21dbe374268086ae268750b9786f6ed9e4779dd1
Adem Efe Devrez <adevrez77@gmail.com>
2023-04-27 17:00:43 +0300

    Add custom sizing, better site style

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-const container = document.querySelector("#container");
-container.setAttribute('style', 'display: flex; width: 500px; height: 500px; flex-wrap: wrap; flex-direction: column; background-color: gray;');
-
-for(let i = 0; i < 4; i++) {
-  const section = document.createElement('div');
-  section.classList.add('section');
-  container.appendChild(section);
-  for(let j = 0; j < 4; j++) {
-    const div = document.createElement('div');
-    div.classList.add('block');
-    section.appendChild(div);
-    div.addEventListener('mouseenter', () => {
-      div.classList.toggle('active');
-    });
+function generator(number) {
+  for(let i = 0; i < number; i++) {
+    const section = document.createElement('div');
+    section.classList.add('section');
+    section.setAttribute('style', `height: ${100/number}%;`);
+    container.appendChild(section);
+    for(let j = 0; j < number; j++) {
+      const div = document.createElement('div');
+      div.classList.add('block');
+      div.setAttribute('style', `width: ${100/number}%;`);
+      section.appendChild(div);
+      div.addEventListener('mouseenter', () => {
+        div.classList.toggle('active');
+      });
+    }
   }
 }
+
+const container = document.querySelector("#container");
+container.setAttribute('style', 'display: flex; width: 400px; height: 400px; flex-wrap: wrap; flex-direction: column; background-color: #f2f2f2;');
+
+let number = 4;
+
+generator(number);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  do {
+    number = Number(prompt("Enter number between 1 and 100:"));
+  }
+  while(number < 1 || number > 100);
+
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  generator(number);
+});
